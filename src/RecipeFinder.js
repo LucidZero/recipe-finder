0981f14c242bac139f240ff28e1bbba61235eda0
Aleksandar Pavlovic <sweetsare42@gmail.com>
2023-05-13 14:48:58 +0200

    [Refactor] Update RecipeFinder component and handle form submission
    This commit refactors the RecipeFinder component to improve its functionality and handle form submission. The following changes were made:
    
    Reorganized the component code for better readability and maintainability.
    Renamed the maxPreparationTime state variable to maxReadyTime to align with the API request parameter.
    Removed the unused excludeItems state and related functions.
    Updated the handleSubmit function to use the correct variable names for API key and ingredients.
    Joined the includeItems array into a comma-separated string for the includeIngredients API parameter.
    Updated the input field for maximum preparation time to use the maxReadyTime state and the onChange event handler to update the value.
    Moved the submit button to the right of the maximum preparation time input field for better user experience.
    These changes ensure that the form data is correctly passed to the API request, allowing users to find recipes based on the included items and maximum preparation time.

--- src/RecipeFinder.js
+++ src/RecipeFinder.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
 import './RecipeFinder.css';
+import apiKey from './api';
 
 function RecipeFinder() {
   const [includeItems, setIncludeItems] = useState([]);
   const [excludeItems, setExcludeItems] = useState([]);
   const [maxPreparationTime, setMaxPreparationTime] = useState('');
+  const [recipes, setRecipes] = useState([]);
 
   const handleIncludeItemAdd = (event) => {
     if (event.key === 'Enter' && event.target.value.trim() !== '') {
       const newItem = event.target.value.trim();
-      setIncludeItems([...includeItems, newItem]);
+      if (!includeItems.includes(newItem)) {
+        setIncludeItems([...includeItems, newItem]);
+      }
       event.target.value = '';
     }
   };
@@ -17,7 +21,9 @@ function RecipeFinder() {
   const handleExcludeItemAdd = (event) => {
     if (event.key === 'Enter' && event.target.value.trim() !== '') {
       const newItem = event.target.value.trim();
-      setExcludeItems([...excludeItems, newItem]);
+      if (!excludeItems.includes(newItem)) {
+        setExcludeItems([...excludeItems, newItem]);
+      }
       event.target.value = '';
     }
   };
@@ -34,59 +40,85 @@ function RecipeFinder() {
     setMaxPreparationTime(event.target.value);
   };
 
-  return (
-    <div className="recipe-finder">
-      <h2>Recipe Finder</h2>
-      <div className="options">
-        <div className="option">
-          <label htmlFor="includeItems">Include Items:</label>
-          <input
-            type="text"
-            id="includeItems"
-            onKeyDown={handleIncludeItemAdd}
-            placeholder="Type and press Enter"
-          />
-          <div className="tags">
-            {includeItems.map((item) => (
-              <div key={item} className="tag">
-                {item}
-                <button onClick={() => handleIncludeItemRemove(item)}>x</button>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="option">
-          <label htmlFor="excludeItems">Exclude Items:</label>
-          <input
-            type="text"
-            id="excludeItems"
-            onKeyDown={handleExcludeItemAdd}
-            placeholder="Type and press Enter"
-          />
-          <div className="tags">
-            {excludeItems.map((item) => (
-              <div key={item} className="tag">
-                {item}
-                <button onClick={() => handleExcludeItemRemove(item)}>x</button>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="option">
-          <label htmlFor="maxPreparationTime">Maximum Preparation Time (minutes):</label>
-          <input
-            type="number"
-            id="maxPreparationTime"
-            min="0"
-            max="2850"
-            value={maxPreparationTime}
-            onChange={handleMaxPreparationTimeChange}
-          />
+  const handleSubmit = () => {
+    const api_key = apiKey; // Use a different variable name here
+    const includeIngredientsString = includeItems.join(',');
+    const excludeIngredientsString = excludeItems.join(',');
+    const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?includeIngredients=${encodeURIComponent(includeIngredientsString)}&excludeIngredients=${encodeURIComponent(excludeIngredientsString)}&maxReadyTime=${maxPreparationTime}&number=2&apiKey=${api_key}`;
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setRecipes(data.results);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+
+return (
+  <div className="recipe-finder">
+    <h2>Recipe Finder</h2>
+    <div className="options">
+      <div className="option">
+        <label htmlFor="includeItems">Include Items:</label>
+        <input
+          type="text"
+          id="includeItems"
+          onKeyDown={handleIncludeItemAdd}
+          placeholder="Type and press Enter"
+        />
+        <div className="tags">
+          {includeItems.map((item) => (
+            <div key={item} className="tag">
+              {item}
+              <button onClick={() => handleIncludeItemRemove(item)}>x</button>
+            </div>
+          ))}
         </div>
       </div>
-      {/* Add code here to fetch and display recipes based on the selected options */}
+      <div className="option">
+        <label htmlFor="excludeItems">Exclude Items:</label>
+        <input
+          type="text"
+          id="excludeItems"
+          onKeyDown={handleExcludeItemAdd}
+          placeholder="Type and press Enter"
+        />
+        <div className="tags">
+          {excludeItems.map((item) => (
+            <div key={item} className="tag">
+              {item}
+              <button onClick={() => handleExcludeItemRemove(item)}>x</button>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="option">
+        <label htmlFor="maxPreparationTime">Maximum Preparation Time (minutes):</label>
+        <input
+          type="number"
+          id="maxPreparationTime"
+          min="0"
+          max="2850"
+          value={maxPreparationTime}
+          onChange={handleMaxPreparationTimeChange}
+        />
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
     </div>
-  );
+    <div className="recipes">
+      {recipes.map((recipe) => (
+        <div key={recipe.id} className="recipe">
+          <img src={recipe.image} alt={recipe.title} />
+          <h3>{recipe.title}</h3>
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
+
 }
 
 export default RecipeFinder;

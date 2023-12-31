3da2140019eb810c11eab96e24c10416c4f10472
Aleksandar Pavlovic <sweetsare42@gmail.com>
2023-12-31 13:22:45 +0100

    Seems hashrouter won't help with 404 error this time.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import RecipeFinder from './RecipeFinder';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Router basename="/">
       <div className="App">
         <RecipeFinder />
       </div>
-    </Router>
   );
 }
 
